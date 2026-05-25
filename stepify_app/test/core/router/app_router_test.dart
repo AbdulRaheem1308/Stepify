584b6bd86639bb79f32e7224fe70ac4015191207
AbdulRaheem1308 <raheemc1308@gmail.com>
2026-05-25 15:51:40 +0530

    test: fix StorageService uninitialized error in app_router_test.dart by setting up Hive

--- stepify_app/test/core/router/app_router_test.dart
+++ stepify_app/test/core/router/app_router_test.dart
@@ -3,7 +3,21 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:stepify_app/core/router/app_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'dart:io';
+import 'package:hive/hive.dart';
+import 'package:stepify_app/services/storage_service.dart';
+
 void main() {
+  setUpAll(() async {
+    final path = '${Directory.current.path}/test/hive_testing_path';
+    Hive.init(path);
+    await StorageService.init();
+  });
+
+  tearDownAll(() async {
+    await Hive.deleteFromDisk();
+  });
+
   group('AppRouter', () {
     testWidgets('pumps the router and navigates to basic routes', (tester) async {
       await tester.pumpWidget(
