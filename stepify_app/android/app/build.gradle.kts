1bf6af1b2e44648b75e5c32159b2d04a27e62078
AbdulRaheem1308 <raheemc1308@gmail.com>
2026-05-25 19:22:12 +0530

    build(android): force espresso and runner versions to fix android test build constraint issues

--- stepify_app/android/app/build.gradle.kts
+++ stepify_app/android/app/build.gradle.kts
@@ -79,3 +79,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+configurations.all {
+    resolutionStrategy {
+        force("androidx.test:runner:1.5.2")
+        force("androidx.test.espresso:espresso-core:3.5.1")
+    }
+}
