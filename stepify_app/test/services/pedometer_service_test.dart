f9d19e416ed6428ec2c66d316aa55b7eaf0576e5
AbdulRaheem1308 <raheemc1308@gmail.com>
2026-05-25 15:28:50 +0530

    fix: add missing foundation import to pedometer_service_test.dart

--- stepify_app/test/services/pedometer_service_test.dart
+++ stepify_app/test/services/pedometer_service_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/foundation.dart';
 import 'package:stepify_app/services/pedometer_service.dart';
 import 'package:stepify_app/services/storage_service.dart';
 import 'package:hive_flutter/hive_flutter.dart';
