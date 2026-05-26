7666fb557c400b1cdc900dee4b97d61ed8dd73d0
AbdulRaheem1308 <raheemc1308@gmail.com>
2026-05-27 00:46:04 +0530

    fix(app): mock ApiService and init Hive/StorageService in main_test.dart

--- stepify_app/test/main_test.dart
+++ stepify_app/test/main_test.dart
@@ -1,11 +1,75 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:stepify_app/main.dart';
 import 'package:stepify_app/core/router/app_router.dart';
+import 'package:stepify_app/services/api_service.dart';
+import 'package:stepify_app/services/storage_service.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:dio/dio.dart';
+
+class MockApiService extends ApiService {
+  MockApiService() : super();
+
+  @override
+  Future<Response<dynamic>> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+    CancelToken? cancelToken,
+  }) async {
+    return Response(
+      requestOptions: RequestOptions(path: path),
+      data: {
+        'themeMode': 'system',
+        'language': 'en',
+        'pushNotifications': true,
+        'dailyReminders': true,
+        'dataSyncOverCellular': true,
+        'soundEnabled': true,
+        'isPublic': true,
+        'showOnLeaderboard': true,
+        'showMilestones': true,
+        'distanceUnit': 'km',
+      },
+      statusCode: 200,
+    );
+  }
+
+  @override
+  Future<Response<dynamic>> put(
+    String path, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    CancelToken? cancelToken,
+  }) async {
+    return Response(
+      requestOptions: RequestOptions(path: path),
+      data: {},
+      statusCode: 200,
+    );
+  }
+}
 
 void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
+            const MethodChannel('be.tramckrijte.workmanager/workmanager'),
+            (MethodCall methodCall) async {
+      return null;
+    });
+
+    final temp = await Directory.systemTemp.createTemp();
+    Hive.init(temp.path);
+    if (!Hive.isBoxOpen('stepify_storage')) {
+      await StorageService.init();
+    }
+  });
+
   testWidgets('StepifyApp creates successfully', (WidgetTester tester) async {
     final mockRouter = GoRouter(
       initialLocation: '/',
@@ -19,6 +83,7 @@ void main() {
       ProviderScope(
         overrides: [
           appRouterProvider.overrideWithValue(mockRouter),
+          apiServiceProvider.overrideWithValue(MockApiService()),
         ],
         child: const StepifyApp(),
       ),
